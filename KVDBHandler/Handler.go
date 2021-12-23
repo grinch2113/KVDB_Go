afbfc0811dcd77a8878fb2e76e947aebc896ebf7
HP <1755982205@qq.com>
2021-12-24 00:57:51 +0800

    完成了迭代1

--- KVDBHandler/Handler.go
+++ KVDBHandler/Handler.go
@@ -1,18 +1,67 @@
 package KVDBHandler
 
-func create() {
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+)
 
+const path = "./Database/"
+
+var dbName = "MyDB.kvdb"
+
+func init() {
+	os.MkdirAll(path, os.ModePerm)
+	if _, err := os.Stat(path + dbName); os.IsNotExist(err) {
+		os.Create(path + dbName)
+	}
 }
 
-func set() {
-
+// Set 写入一条记录/**
+func Set(key, value string) {
+	file, err := os.OpenFile(path+dbName, os.O_APPEND, 0666)
+	defer file.Close()
+	if err != nil {
+		fmt.Println("文件打开错误：" + err.Error())
+		return
+	}
+	_, err = file.Write([]byte(key + " " + value + "\n"))
+	if err != nil {
+		fmt.Println("文件写入错误：" + err.Error())
+		return
+	}
 }
 
-func get() {
-
+// Get 读取一条记录/**
+func Get(key string) string {
+	file, err := os.Open(path + dbName)
+	if err != nil {
+		fmt.Println("打开文件失败：" + err.Error())
+		return ""
+	}
+	defer file.Close()
+	value := ""
+	reader := bufio.NewReader(file)
+	for {
+		line, err := reader.ReadString('\n')
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			fmt.Println("读取文件错误：" + err.Error())
+			return ""
+		}
+		kvGroup := strings.Split(line, " ")
+		if kvGroup[0] == key {
+			value = kvGroup[1]
+		}
+	}
+	return value
 }
 
-func remove() {
+func Remove() {
 
 }
 
--- main.go
+++ main.go
@@ -1,5 +1,44 @@
 package main
 
+import (
+	handler "GO_KVDB/KVDBHandler"
+	"fmt"
+)
+
+func help() {
+	fmt.Println("welcome to kv database system")
+	fmt.Println("1. set a k-v group into current database")
+	fmt.Println("2. get a value by key from current database")
+	fmt.Println("3. quit")
+	fmt.Println(`type "help" to get options`)
+}
 func main() {
+	help()
+	for {
+		var option string
+		fmt.Scanln(&option)
+		switch option {
+		case "1":
+			var key, value string
+			fmt.Scanln(&key, &value)
+			handler.Set(key, value)
+		case "2":
+			var key string
+			fmt.Scanln(&key)
+			value := handler.Get(key)
+			if len(value) == 0 {
+				fmt.Println("未找到对应的value")
+			} else {
+				fmt.Println(value[:len(value)-1])
+			}
+
+		case "3":
+			return
+		case "help":
+			help()
+		default:
+			fmt.Println("invalid option, type 5 to get options")
+		}
+	}
 
 }
