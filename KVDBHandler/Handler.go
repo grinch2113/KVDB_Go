9ea270a31bd04242fedb7c9c828381a650c9e038
HP <1755982205@qq.com>
2022-01-08 14:41:36 +0800

    补充了路径声明和init函数

--- KVDBHandler/Handler.go
+++ KVDBHandler/Handler.go
@@ -1,5 +1,18 @@
 package KVDBHandler
 
+import "os"
+
+const path = "./Database/"
+
+const dbName = "MyDB.kvdb"
+
+func init() {
+	os.MkdirAll(path, os.ModePerm)
+	if _, err := os.Stat(path + dbName); os.IsNotExist(err) {
+		os.Create(path + dbName)
+	}
+}
+
 func create() {
 
 }
